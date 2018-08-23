c0bd1c21365479c5dc4da951f9cff3a7fa7a74f9
Jason Knee <nghejason@gmail.com>
2018-08-23 09:41:45 -1000

    added ProductsController and Views

--- SugarTits.FundingAvenue/Controllers/FAQController.cs
+++ SugarTits.FundingAvenue/Controllers/FAQController.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SugarTits.FundingAvenue.Controllers
 {
-    public class FnQController : Controller
+    public class FAQController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
