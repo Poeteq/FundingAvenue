353bb0d09b96aa9d85d5502e517dabafb9ec52ac
jason nghe <nghejason@gmail.com>
2018-09-09 15:34:56 -0700

    wired up homepage to ctrl

---
+++ Poeteq.FundingAvenue/wwwroot/app/controllers/home.controller.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('FundingAvenue')
+        .controller('HomeController', HomeController);
+
+    /* @ngInject */
+    function HomeController($window) {
+        var vm = this;
+
+        vm.navigateToTier1 = navigateToTier1;
+        vm.navigateToTier2 = navigateToTier2;
+        vm.navigateToTier3 = navigateToTier3;
+
+        function navigateToTier1() {
+            $window.location.href = "/program/tier1";
+        }
+
+        function navigateToTier2() {
+            $window.location.href = "/program/tier2";
+
+        }
+
+        function navigateToTier3() {
+            $window.location.href = "/program/tier3";
+        }
+
+    }
+})();
