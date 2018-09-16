c26fca063cfa37392fede5f244ee309db7a703b0
jason nghe <nghejason@gmail.com>
2018-09-15 18:50:26 -0700

    homepage fix

--- Poeteq.FundingAvenue/wwwroot/app/controllers/home.controller.js
+++ Poeteq.FundingAvenue/wwwroot/app/controllers/home.controller.js
@@ -8,28 +8,13 @@
     /* @ngInject */
     function HomeController($window) {
         var vm = this;
+        
+        vm.navigateToProduct = navigateToProduct;
 
-        vm.navigateToTier1 = navigateToTier1;
-        vm.navigateToTier2 = navigateToTier2;
-        vm.navigateToTier3 = navigateToTier3;
-        vm.navigateToBusinessCreditLines = navigateToBusinessCreditLines;
-
-        function navigateToBusinessCreditLines() {
-            $window.location.href = "/product/businesscreditlines"
+        function navigateToProduct(product) {
+            $window.location.href = "/product/" + product;
         }
-
-
-        function navigateToTier1() {
-            $window.location.href = "/products/tier1";
-        }
-
-        function navigateToTier2() {
-            $window.location.href = "/products/tier2";
-        }
-
-        function navigateToTier3() {
-            $window.location.href = "/products/tier3";
-        }
+        
     }
 
 })();
