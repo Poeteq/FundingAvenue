3dee31e737d67863d43dea18521d3df03646932a
jason nghe <nghejason@gmail.com>
2018-09-15 18:07:50 -0700

    Alternative Funding bg pic and logic [v0.06]

--- Poeteq.FundingAvenue/wwwroot/app/controllers/alternative.controller.js
+++ Poeteq.FundingAvenue/wwwroot/app/controllers/alternative.controller.js
@@ -35,24 +35,37 @@
         function calculateProduct() {
             if (vm.products.one && !vm.products.two && !vm.products.three) {
                 vm.recommendation = "Business Revolving Credit Lines";
+                vm.productLink = "/product/businesscreditlines";
+                vm.isAlternative = false;
             }
             else if (!vm.products.one && vm.products.two && !vm.products.three) {
                 vm.recommendation = "Personal Revolving Credit Lines";
+                vm.productLink = "/product/personalcreditlines";
+                vm.isAlternative = false;
             }
             else if (!vm.products.one && !vm.products.two && vm.products.three) {
                 vm.recommendation = "Personal Cash Loans";
+                vm.productLink = "/product/personalloans";
+                vm.isAlternative = false;
             }
             else if (vm.products.one && vm.products.two && !vm.products.three) {
                 vm.recommendation = "Alternative A";
+                vm.isAlternative = true;
+                vm.id = "#alt-a";
             }
-            else if (vm.products.one && !vm.products.two && vm.products.three) {
+            else if ((vm.products.one && !vm.products.two && vm.products.three) || (!vm.products.one && vm.products.two && vm.products.three)) {
                 vm.recommendation = "Alternative B";
+                vm.isAlternative = true;
+                vm.id = "#alt-b";
             }
-            else if (!vm.products.one && vm.products.two && vm.products.three) {
-                vm.recommendation = "Alternative B";
-            }
+            //else if (!vm.products.one && vm.products.two && vm.products.three) {
+            //    vm.recommendation = "Alternative B";
+            //    vm.isAlternative = false;
+            //}
             else if (vm.products.one && vm.products.two && vm.products.three) {
                 vm.recommendation = "Alternative C";
+                vm.isAlternative = true;
+                vm.id = "#alt-c";
             }
             else {
                 vm.recommendation = null;
