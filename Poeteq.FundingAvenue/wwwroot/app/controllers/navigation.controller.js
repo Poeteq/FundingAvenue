6e36cb8d2e2f4f3a45407c5e675568f8a9d9f700
Jason Knee <nghejason@gmail.com>
2018-09-30 10:28:01 -0700

    [v0.22] UI Fixes

---
+++ Poeteq.FundingAvenue/wwwroot/app/controllers/navigation.controller.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('FundingAvenue')
+        .controller('NavigationController', NavigationController);
+
+    NavigationController.$inject = ['$window'];
+    function NavigationController($window) {
+        var vm = this;
+
+        vm.navigateTo = navigateTo;
+
+        function navigateTo(page) {
+            $window.location.href = page;
+        }
+    }
+})();
